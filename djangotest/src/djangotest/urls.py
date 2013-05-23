b56f466185064961a98f45cc8df484face820a00
Steven Bluen <sbluen153@yahoo.com>
2013-05-22 20:59:19 -0700

    steps 1-4 of tutorial

--- djangotest/src/djangotest/urls.py
+++ djangotest/src/djangotest/urls.py
@@ -6,24 +6,15 @@ from polls import views
 from django.contrib import admin
 admin.autodiscover()
 
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'djangotest.views.home', name='home'),
-    # url(r'^djangotest/', include('djangotest.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-    
-    # ex: /polls/
-    url(r'^$', views.index, name='index'),
-    url(r'polls', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
-    url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
+
+
+
--- djangotest/src/polls/models.py
+++ djangotest/src/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
@@ -6,6 +8,8 @@ class Poll(models.Model):
     
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.question
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
