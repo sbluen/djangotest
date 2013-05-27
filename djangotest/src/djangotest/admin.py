b788cd1a870c58d9c8e08ea0fe2df06267b8f506
Steven Bluen <sbluen153@yahoo.com>
2013-05-27 16:25:12 -0700

    admin interface

--- djangotest/src/djangotest/admin.py
+++ djangotest/src/djangotest/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from polls.models import Choice, Poll
+from forum.models import Forum, Topic, Post
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -11,5 +12,21 @@ class PollAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-
+    
 admin.site.register(Poll, PollAdmin)
+
+# class ForumAdmin(admin.ModelAdmin):
+#     fields = ['id', 'name']
+# admin.site.register(Forum, ForumAdmin)
+# 
+# class TopicAdmin(admin.ModelAdmin):
+#     fields = ['id', 'title' 'forum_id']
+# admin.site.register(Topic, TopicAdmin)
+# 
+# class PostAdmin(admin.ModelAdmin):
+#     fields = ['id', 'title', 'topic_id']
+# admin.site.register(Topic, TopicAdmin)
+
+admin.site.register(Forum)
+admin.site.register(Topic)
+admin.site.register(Post)
